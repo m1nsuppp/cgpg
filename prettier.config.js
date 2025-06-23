286b462be0d867dd8e9fa930fee2d2be1801f2ce
Minsup Lee <m1nsuppp@gmail.com>
2025-06-24 00:23:23 +0900

    chore: config prettier, eslint

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+import m1nsuppp from '@m1nsuppp/prettier-config';
+
+/** @type {import('prettier').Config} */
+export default {
+  ...m1nsuppp,
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { App } from './_app';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+(() => {
+  const root = document.getElementById('root');
+  if (root === null) {
+    throw new Error('Root element not found');
+  }
+
+  createRoot(root).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+})();
