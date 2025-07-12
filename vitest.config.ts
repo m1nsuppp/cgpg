aa98be4e9ec3ac552d48088197499f9489e1a584
Minsup Lee <m1nsuppp@gmail.com>
2025-07-13 02:26:58 +0900

    feat: init

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, defineProject, mergeConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import reactPlugin from '@vitejs/plugin-react';
 
 const baseConfig = defineConfig({
   test: {
@@ -32,7 +31,6 @@ const uiConfig = mergeConfig(
 export default mergeConfig(
   uiConfig,
   defineConfig({
-    plugins: [reactPlugin()],
     test: {
       setupFiles: ['./vitest.setup.ts'],
     },
